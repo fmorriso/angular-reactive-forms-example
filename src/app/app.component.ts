365337773270f1613cdc98fb9ef1b6c0e811743b
Frederick Morrison <frederick.morrison@outlook.com>
2023-04-24 16:35:49 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, VERSION as ngVersion } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+// @ts-ignore
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FormsExample';
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({}, null);
+  }
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  private initializeForm(): void {
+    this.myForm = this.fb.group({
+      name: ['Sammy', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(15)]],
+    });
+  }
+
+  public get AngularVersion(): string { return ngVersion.full;}
+
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid); // true or false
+    console.log('Name', form.value.name);
+    console.log('Email', form.value.email);
+    console.log('Message', form.value.message);
+  }
 }
