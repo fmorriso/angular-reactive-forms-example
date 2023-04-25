700aec0ee233dde0fd549dec299268dab17895c7
Frederick Morrison <frederick.morrison@outlook.com>
2023-04-25 11:20:37 -0600

    add Custom Link interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { MaterialFormComponent } from './material-form/material-form.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: 'reactive', component: ReactiveFormComponent},
-  {path: 'material', component: MaterialFormComponent},
-  {path: 'about', component: AboutComponent},
+  {path: 'reactive', component: ReactiveFormComponent, data: {label: 'Reactive'}},
+  {path: 'material', component: MaterialFormComponent, data: {label: 'Material'}},
+  {path: 'about', component: AboutComponent, data: {label: 'About'}},
   {path: '', redirectTo: '/about', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
 ];
 const routeOptions: ExtraOptions = {enableTracing: true};
 
